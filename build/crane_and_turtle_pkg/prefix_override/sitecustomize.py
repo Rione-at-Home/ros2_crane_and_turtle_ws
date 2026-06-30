ac3cdf82c2e44d0dda22e851b1dc9fed044b9cb3
Gil-gil-glitch <gilsocojp@gmail.com>
2026-06-30 13:43:46 +0900

    Added gitignore file

--- build/crane_and_turtle_pkg/prefix_override/sitecustomize.py
+++ build/crane_and_turtle_pkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ri-one/crane_and_turtle_ws/install/crane_and_turtle_pkg'
+    sys.prefix = sys.exec_prefix = '/home/ri-one/ros2_crane_and_turtle_ws/install/crane_and_turtle_pkg'
