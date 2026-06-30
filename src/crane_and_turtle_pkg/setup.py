9a2b5598f9d3f5adb981c8d76fa20efa329e0077
Gil-gil-glitch <gilsocojp@gmail.com>
2026-06-30 14:09:34 +0900

    Updated the entry point for the challenge_node

--- src/crane_and_turtle_pkg/setup.py
+++ src/crane_and_turtle_pkg/setup.py
@@ -26,6 +26,8 @@ setup(
         'console_scripts': [
             'driver_node = crane_plus_driver.crane_driver_node:main',
             'gui_node = crane_plus_driver.gui:main',
+            'challenge_node = crane_and_turtle_pkg.challenge_node:main',
+            
         ],
     },
 )
